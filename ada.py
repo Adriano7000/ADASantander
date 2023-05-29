d1f7c076fff70d2938d57ad669865ecd7b9341f1
Adriano Dias <adrianodias7000@gmail.com>
2023-05-29 02:34:42 -0300

    adding new algorithm exercise files.

---
+++ ada.py
@@ -0,0 +1,15 @@
+a = 0.0
+
+x = int(input("Digite um numero: "))
+
+i =0
+t =0
+
+while i < x:
+    e = int(input("escreva um numero: "))
+    t = t+e
+    i = i+1
+    
+a =t/x 
+
+print(f"{i} {a}")
